b87815eb3db5ff807220c9d80df31c6ed2a5d7b9
Marcia Paige <mapaige7@gmail.com>
2020-08-09 15:18:27 -0400

    Added delete function

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,17 +1,34 @@
 var db = require("../db/db.json")
-var fs = require("fs")
+var fs = require("fs");
+//  const { notStrictEqual } = require("assert");
 function apiroutes(app){
   app.get("/api/notes", function(req, res){
     res.json(db)
   })
   app.post("/api/notes",function(req,res){
-    db.push(req.body)
+    let newNote= req.body
+    newNote.id = Math.floor(Math.random() * Math.floor(10000));
+    db.push(newNote)
     console.log(req.body)
+    fs.writeFile("./db/db.json",JSON.stringify(db),function(){
+      res.json(db)
+  })
+})
+  app.delete("/api/notes/:id",function(req,res){
+    let noteId =parseInt(req.params.id)
+    console.log(noteId)
+    res.json(true);
+    const result = db.filter(item => item.id !== noteId);
+    console.log(result)
+    db= result
     fs.writeFile("./db/db.json",JSON.stringify(db),function(){
       res.json(db)
     })
 
-  })
+
+  });
 }
+
+
 
 module.exports = apiroutes
